93629e7835f494fc3edbdc3ff168939009c076de
youngbear <youngbear@aliyun.com>
2019-02-21 21:53:38 +0800

    init repo
    Signed-off-by: youngbear <youngbear@aliyun.com>

---
+++ src/main/java/com/example/jsondemo/JsonDemo.java
@@ -0,0 +1,15 @@
+package com.example.jsondemo;
+
+/**
+ * @author youngbear
+ * @email youngbear@aliyun.com
+ * @date 2019/2/21 21:48
+ * @blog https://blog.csdn.net/next_second
+ * @github https://github.com/YoungBear
+ * @description
+ */
+public class JsonDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Json");
+    }
+}
